08b626795e613385806516744a7144b79f84fafa
Mike Macfadden <mikemacfadden@gmail.com>
2023-04-21 20:58:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const quotesOfTheDay = [
     {
     date: "04242023",
@@ -32,16 +33,41 @@ const quotesOfTheDay = [
   {
     date: "04192023",
     quote: "The most valuable commodity I know of is information. Wouldn't you agree?",
+=======
+// Get today's date in the format of "Month day, year"
+const today = new Date().toLocaleDateString("en-US", {
+  month: "long",
+  day: "numeric",
+  year: "numeric",
+});
+
+const quotesOfTheDay = [
+  {
+    date: "April 20, 2023",
+    quote: "If peeing your pants is cool, consider me Miles Davis",
+    id: 11017,
+    src: "https://www.youtube.com/embed/siCNdfH9U40",
+  },
+  {
+    date: "April 19, 2023",
+    quote:
+      "The most valuable commodity I know of is information. Wouldn't you agree?",
+>>>>>>> Stashed changes
     id: 10673,
     src: "https://www.youtube.com/embed/7tZBkOtIYkU?start=13&end=20",
   },
   {
+<<<<<<< Updated upstream
     date: "04182023",
+=======
+    date: "April 18, 2023",
+>>>>>>> Stashed changes
     quote: "Stop chasing me!",
     id: 640,
     src: "https://www.youtube.com/embed/pRi-o9-utSE?start=58&end=65",
   },
   {
+<<<<<<< Updated upstream
     date: "0172023",
     quote: "What, I thought we were in the trust tree in the nest, were we not?",
     id: 11635,
@@ -106,16 +132,35 @@ function setRandomDate() {
 }
 
 
+=======
+    date: "April 17, 2023",
+    quote:
+      "What, I thought we were in the trust tree in the nest, were we not?",
+    id: 11635,
+    src: "https://www.youtube.com/embed/umkovqvw714?start=81&end=86",
+  },
+];
+
+const todaysQuote = quotesOfTheDay.find((quote) => quote.date === today);
+
+let id;
+
+>>>>>>> Stashed changes
 // Loop through the quotesOfTheDay array
 for (let i = 0; i < quotesOfTheDay.length; i++) {
   // Check if the date property matches today's date
   if (quotesOfTheDay[i].date === today) {
+<<<<<<< Updated upstream
       id = quotesOfTheDay[i].id;
+=======
+    id = quotesOfTheDay[i].id;
+>>>>>>> Stashed changes
     // Do something with the ID, e.g. fetch movie data and display it
     break; // exit the loop since we found a match
   }
 }
 
+<<<<<<< Updated upstream
 
 
 //THE MOVIE DATABASE MAGIC
@@ -134,10 +179,29 @@ searchInput.addEventListener('input', () => {
 
   // Clear any previous suggestions and the addedTitles array
   suggestionsList.innerHTML = '';
+=======
+//THE MOVIE DATABASE MAGIC
+const apiKey = "fe02516c84b34aff3bd02db47d61ec88";
+const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
+
+//SUGGESTIONS ENGINE
+const searchInput = document.getElementById("guess");
+const suggestionsList = document.getElementById("suggestions");
+const container = document.getElementById("guess-form");
+
+let addedTitles = []; // keep track of the titles that have already been added to the list
+
+searchInput.addEventListener("input", () => {
+  const query = searchInput.value;
+
+  // Clear any previous suggestions and the addedTitles array
+  suggestionsList.innerHTML = "";
+>>>>>>> Stashed changes
   addedTitles = [];
 
   // Make a request to the Movie Database API to get movie suggestions based on the user input
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&language=en-US&page=1&include_adult=false`;
+<<<<<<< Updated upstream
   
   fetch(url)
     .then(response => response.json())
@@ -148,6 +212,18 @@ searchInput.addEventListener('input', () => {
       results.forEach(movie => {
         if (!addedTitles.includes(movie.title) && addedTitles.length < 3) {
           const listItem = document.createElement('li');
+=======
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const results = data.results.slice(0, 3); // Get the top 3 results
+
+      // Create a list item for each suggestion and append it to the suggestions list if its title has not already been added
+      results.forEach((movie) => {
+        if (!addedTitles.includes(movie.title) && addedTitles.length < 3) {
+          const listItem = document.createElement("li");
+>>>>>>> Stashed changes
           listItem.textContent = movie.title;
           suggestionsList.appendChild(listItem);
           addedTitles.push(movie.title);
@@ -156,6 +232,7 @@ searchInput.addEventListener('input', () => {
 
       // Display the suggestions list if there are suggestions, hide it otherwise
       if (addedTitles.length > 0) {
+<<<<<<< Updated upstream
         suggestionsList.style.display = 'block';
       } else {
         suggestionsList.style.display = 'none';
@@ -163,9 +240,119 @@ searchInput.addEventListener('input', () => {
     })
     .catch(error => {
       console.error(error);
+=======
+        suggestionsList.style.display = "block";
+      } else {
+        suggestionsList.style.display = "none";
+      }
+    })
+    .catch((error) => {
+      console.error(error);
     });
 });
 
+// When a suggestion is clicked, populate the search input with the movie title and hide the suggestions list
+suggestionsList.addEventListener("click", (event) => {
+  const clickedItem = event.target;
+  const selectedTitle = clickedItem.textContent;
+  searchInput.value = selectedTitle;
+  suggestionsList.style.display = "none";
+});
+
+// Hide the suggestions box when the user clicks outside of it
+document.addEventListener("click", (event) => {
+  if (!container.contains(event.target)) {
+    suggestionsList.style.display = "none";
+  }
+});
+
+//DAILY DATA STUFF
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const title = data.title;
+    const tagline = data.tagline;
+    const release_date = data.release_date;
+    const runtime = data.runtime;
+    const overview = data.overview;
+    const posterPath = data.poster_path;
+    const posterUrl = "https://image.tmdb.org/t/p/w500" + posterPath;
+
+    var elements = document.getElementsByClassName("title");
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].textContent = title;
+    }
+    var elements = document.getElementsByClassName("overview");
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].textContent = overview;
+    }
+    document.getElementById("poster").setAttribute("src", posterUrl);
+
+    //DISPLAY TODAY'S DATE AND QUOTE
+    document.getElementById("quote").textContent =
+      '"' + todaysQuote.quote + '"';
+    document.getElementById("date").textContent = todaysQuote.date;
+
+    //YOUTUBE
+    document.getElementById("youTube").src = todaysQuote.src;
+
+    const form = document.getElementById("guess-form");
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent the form from submitting
+
+      // Get the user's answer from the input box
+      const userAnswer = document.getElementById("guess").value;
+      if (success == false) {
+        numberOfGuesses = numberOfGuesses + 1;
+      }
+      console.log("Number of Guesses:" + numberOfGuesses.toString());
+      document.getElementById("guess-count").textContent =
+        numberOfGuesses.toString();
+
+      // Check if the user's answer matches the movie title
+      if (userAnswer.toLowerCase().trim() === title.toLowerCase().trim()) {
+        // If the answer is correct, show the "answer" element
+        success = true;
+        document.getElementById("answer").style.height = "auto";
+        document.getElementById("answer").style.opacity = "1";
+        document.getElementById("answer").style.transition = "opacity .3s";
+        const modal = document.getElementById("answerModal");
+        const modalInstance = new bootstrap.Modal(modal);
+        modalInstance.show();
+
+        document.getElementById("guess-count").textcontent = "numberOfGuesses";
+
+        // Get the og:title meta tag
+        var ogTitle = document.querySelector('meta[property="og:title"]');
+        // Set the new title value
+        var newTitle =
+          "Guesses: " +
+          numberOfGuesses.toString() +
+          ", Hints: " +
+          numberOfHints.toString();
+        // Set the content attribute of the og:title meta tag to the new title value
+        ogTitle.setAttribute("content", newTitle);
+
+        // If the answer is incorrect, show an error message
+        document.getElementById("error-message").style.display = "none";
+        document.getElementById("error-message").textContent = "";
+        console.log("foo");
+      } else {
+        // If the answer is incorrect, show an error message
+        console.log("nope");
+        document.getElementById("error-message").style.display = "block";
+        document.getElementById("error-message").textContent =
+          "Sorry, that answer is incorrect. Click above for a hint.";
+        // If the answer is correct, show the "answer" element
+        document.getElementById("answer").style.height = "0";
+        document.getElementById("answer").style.opacity = "0";
+      }
+>>>>>>> Stashed changes
+    });
+});
+
+<<<<<<< Updated upstream
 // When a suggestion is clicked, populate the search input with the movie title and hide the suggestions list
 suggestionsList.addEventListener('click', event => {
   const clickedItem = event.target;
@@ -341,25 +528,73 @@ function hideHint() {
     const hintElement = document.getElementById('hint');
     hintElement.style.display = 'none';
   }
+=======
+    const hints = [release_date, runtime, tagline, overview]; // Array of hints
+    const hintsCount = hints.length;
+    var success = false;
+    let currentHint = 0; // Current hint index
+    const closeButton =
+      '<span aria-hidden="true"><i class="bi bi-x-circle close hint-exit" aria-label="Close" onclick="hideHint()""></i></span>';
+>>>>>>> Stashed changes
 
+    const hintButton = document.getElementById("hint-button");
+
+    hintButton.addEventListener("click", function () {
+      // Your code here
+      console.log("Hint clicked!");
+
+      if (numberOfHints < hintsCount) {
+        numberOfHints = numberOfHints + 1;
+      }
+      console.log("Number of Hints:" + numberOfHints.toString());
+      document.getElementById("hint-count").textContent =
+        numberOfHints.toString();
+
+      const hintElement = document.getElementById("hint");
+      hintElement.style.display = "block";
+
+      // Set the hint text to the current hint and increment the counter
+      const hintLabels = ["Release Date:", "Runtime:", "Tagline:", "Overview:"]; // Array of hint labels
+      hintElement.innerHTML =
+        closeButton +
+        "<strong>" +
+        hintLabels[currentHint] +
+        "</strong> " +
+        hints[currentHint];
+
+      currentHint = (currentHint + 1) % hints.length; // Wrap around to the beginning of the array if necessary
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+function hideHint() {
+  const hintElement = document.getElementById("hint");
+  hintElement.style.display = "none";
+}
 
 var numberOfHints = 0;
 var numberOfGuesses = 0;
 
+<<<<<<< Updated upstream
 //STOP YOUTUBE VIDEO WHEN MODAL CLOSES
 var myModal = document.getElementById('answerModal');
 myModal.addEventListener('hide.bs.modal', function (event){
   document.getElementById("youTube").src = todaysQuote.src;
 })
 
+=======
+>>>>>>> Stashed changes
 //WEB SHARE API
-const shareButton = document.querySelector('#share-button');
-const imgElement = document.querySelector('#image');
+const shareButton = document.querySelector("#share-button");
+const imgElement = document.querySelector("#image");
 
 if (navigator.share) {
-  shareButton.addEventListener('click', async () => {
+  shareButton.addEventListener("click", async () => {
     try {
       // Fetch the image file and convert it to a Blob
+<<<<<<< Updated upstream
       if (numberOfGuesses == 1 && numberOfHints == 0){
         var response = await fetch('img/results/G1H0.png');
       } else if(numberOfGuesses == 1 && numberOfHints == 1){
@@ -456,21 +691,44 @@ if (navigator.share) {
       await navigator.share({
         //title: 'MQOTD',
         //text: 'MQOTD: Guesses: ' + numberOfGuesses.toString() + ', Hints: ' + numberOfHints.toString(),
+=======
+      const response = await fetch("img/og-img.png");
+      const blob = await response.blob();
+
+      // Create a new File object from the Blob
+      const file = new File([blob], "og-img.png", { type: "image/png" });
+
+      // Share the data including the file
+      await navigator.share({
+        title: "MQOTD",
+        text:
+          "MQOTD: Guesses: " +
+          numberOfGuesses.toString() +
+          ", Hints: " +
+          numberOfHints.toString(),
+>>>>>>> Stashed changes
         //url: 'https://example.com/web-share-api',
         files: [file],
       });
 
-      console.log('Successful share');
+      console.log("Successful share");
     } catch (error) {
-      console.log('Error sharing:', error);
+      console.log("Error sharing:", error);
     }
   });
 } else {
   // Web Share API is not supported, provide a fallback option
-  document.getElementById('share-button').addEventListener('click', function() {
-    const textToCopy = 'MQOTD: Guesses: ' + numberOfGuesses.toString() + ', Hints: ' + numberOfHints.toString();
-    navigator.clipboard.writeText(textToCopy)
-    .then(() => alert('Copied stats to clipboard!'))
-    .catch((error) => console.log('Error copying text:', error));
-  });
+  document
+    .getElementById("share-button")
+    .addEventListener("click", function () {
+      const textToCopy =
+        "MQOTD: Guesses: " +
+        numberOfGuesses.toString() +
+        ", Hints: " +
+        numberOfHints.toString();
+      navigator.clipboard
+        .writeText(textToCopy)
+        .then(() => alert("Copied stats to clipboard!"))
+        .catch((error) => console.log("Error copying text:", error));
+    });
 }
